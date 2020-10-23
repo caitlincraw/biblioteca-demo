8c8808130a0ac03ffe401e83772ae14e1c8899ea
Caitlin Crawford <caitlin.crawford16@gmail.com>
2020-10-22 21:08:40 -0400

    Add Step 4

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();   
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
+const mainRouter = require('./routers/main');
+// const authRouter = require('./routers/auth');
 
 app.use("/", express.static(__dirname + "/public"));
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/css", express.static(__dirname + "/css"));
+app.use(bodyParser.json());
+
+app.use(mainRouter);
 
 app.get("/heartbeat", function(req, res) {
     res.json({
@@ -11,6 +22,6 @@ app.get("/heartbeat", function(req, res) {
     });
   });
 
-app.listen(3001, () => {
-    console.log(`Server started at Port ${3001}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server started at Port ${process.env.PORT}`)
 });
